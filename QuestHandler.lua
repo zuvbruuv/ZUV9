a06a96376608c2e632d352e1087460f396bacdc0
zuvbruuv <152209324+zuvbruuv@users.noreply.github.com>
2024-09-27 16:28:37 -0400

    Create QuestHandler.lua

---
+++ QuestHandler.lua
@@ -0,0 +1,13 @@
+return {
+    ["Fortune Teller Quest"] = function()
+        print(Completed!")
+    end,
+
+    ["Test Quest"] = function()
+        print("Completed!")
+    end,
+
+    ["Test Quest1"] = function()
+        print("Completed!")
+    end
+}
