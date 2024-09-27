dcc1fad4f3e2248d39cd54b0eb094e45e843f0f4
zuvbruuv <152209324+zuvbruuv@users.noreply.github.com>
2024-09-27 16:52:28 -0400

    Update QuestHandler.lua

--- QuestHandler.lua
+++ QuestHandler.lua
@@ -1,13 +1,5 @@
 return {
     ["Fortune Teller Quest"] = function()
         print("Completed! 1")
-    end,
-
-    ["Test Quest"] = function()
-        print("Completed! 2")
-    end,
-
-    ["Test Quest1"] = function()
-        print("Completed! 3")
     end
 }
